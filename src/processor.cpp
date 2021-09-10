e03a7c2d8c57c9da21baa5fde0ff7ee5474d776f
accranmer <85885052+accranmer@users.noreply.github.com>
2021-09-10 16:17:43 -0500

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,40 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  
+  std::vector<std::string> current = LinuxParser::CpuUtilization();
+  int user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
+  int totald, idled;
+  
+  user = stoi(current[0]);
+  nice = stoi(current[1]);
+  system = stoi(current[2]);
+  idle = stoi(current[3]);
+  iowait = stoi(current[4]);
+  irq = stoi(current[5]);
+  softirq = stoi(current[6]);
+  steal = stoi(current[7]);
+  guest = stoi(current[8]);
+  guest_nice = stoi(current[9]);
+  
+  PrevIdle = Idle;
+  PrevNonIdle = NonIdle;
+  PrevTotal = Total;
+  
+  Idle = idle + iowait;
+  NonIdle = user + nice + system + irq + softirq + steal;
+  Total = Idle + NonIdle;
+  
+  //Differentiate
+  totald = Total - PrevTotal;
+  idled = Idle - PrevIdle;
+  
+  if (totald == 0){
+  return 0.0;
+  }
+  else{
+    return ((float)(totald - idled))/((float)totald);
+  }
+                               
+}
