6fd3b700027d8a8b58305d15520899df536c1705
accranmer <85885052+accranmer@users.noreply.github.com>
2021-09-13 09:47:39 -0500

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -1,5 +1,7 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
+#include <string>
+#include <vector>
 
 class Processor {
  public:
@@ -7,17 +9,13 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
- int PrevIdle{0};
-  int PrevNonIdle{0};
-  int PrevTotal{0};
-  int Idle{0};
-  int NonIdle{0};
-  int Total{0};int PrevIdle{0};
+  int PrevIdle{0};
   int PrevNonIdle{0};
   int PrevTotal{0};
   int Idle{0};
   int NonIdle{0};
   int Total{0};
+  
 };
 
 #endif
